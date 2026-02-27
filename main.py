f7ecabde43fd69cb606913f64a220e620bb8d38c
SacrificV <blyhyjeancs@gmail.com>
2026-02-27 11:22:36 +0500

    1

---
+++ main.py
@@ -0,0 +1,7 @@
+import calculator
+a = int(input("Enter a number: "))
+b = int(input("Enter a number: "))
+print(calculator.add(a,b))
+print(calculator.subtract(a,b))
+print(calculator.multiply(a,b))
+print(calculator.divide(a,b))
