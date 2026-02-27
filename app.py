93ea8fc3ff333e7f1b17f1aff05b7dff5b122a5f
SacrificV <blyhyjeancs@gmail.com>
2026-02-27 11:41:20 +0500

    2nd task and 3rd task

---
+++ app.py
@@ -0,0 +1,10 @@
+
+from mypackage.geometry import area_circle
+from mypackage.geometry import area_rectangle
+from mypackage.statistics import average
+from mypackage.statistics import maximum
+print(area_circle(6))
+print(area_rectangle(6,2))
+lst = [1,2,3,4,5,6,79,8,12,1]
+print(average(lst))
+print(maximum(lst))
